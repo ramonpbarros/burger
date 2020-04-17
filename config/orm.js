ee12309b966d102eb2886e8ea08f55973235b229
Sorrab <ramonpbarros@gmail.com>
2020-04-16 21:06:56 -0700

    add selectAll method to orm

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,16 @@
 const connection = require("./connection");
+
+const orm = {
+  selectAll: (table, cb) => {
+    connection.query(`SELECT * FROM ??`, table, (err, data) => {
+      if (err) {
+        throw err;
+      }
+      cb(data);
+    });
+  },
+};
+
+module.exports = orm;
+
+// orm.selectAll("burgers", (burgers) => console.log(burgers));

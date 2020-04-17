f6dd9a42d9dd01a44274aba3397b8817d75c069f
Sorrab <ramonpbarros@gmail.com>
2020-04-17 09:49:24 -0700

    add createOne method in orm

--- config/orm.js
+++ config/orm.js
@@ -9,8 +9,17 @@ const orm = {
       cb(data);
     });
   },
+  createOne: (table, rowValue, cb) => {
+    connection.query(`INSERT INTO ?? SET ?`, [table, rowValue], (err, data) => {
+      if (err) {
+        throw err;
+      }
+      cb(data);
+    });
+  },
 };
 
 module.exports = orm;
 
 // orm.selectAll("burgers", (burgers) => console.log(burgers));
+// orm.createOne ("burgers", burger_name, "example"], (err, data) => {}
